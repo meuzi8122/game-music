8f4b8964c4622f7ee4cf70967a84b2659dd40aad
meuzi8122 <kagiakete1234@gmail.com>
2024-11-03 22:56:29 +0900

    tableやめる

---
+++ src/lib/components/song-list.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import PlayButton from "./buttons/play-button.svelte";
+    import type { HandlePlayButtonClick, Song } from "$lib/type";
+
+    const { songs, handlePlayButtonClick } = $props<{ songs: Song[]; handlePlayButtonClick: HandlePlayButtonClick; }>();
+</script>
+
+<div class="flex flex-col">
+    {#each songs as song}
+        <div class="flex justify-between">
+            <div class="flex items-center gap-3">
+                <div class="avatar">
+                    <div class="mask mask-squircle h-14 w-14">
+                        <img src={song.artworkUrl} alt={song.title} />
+                    </div>
+                </div>
+                <div>
+                    <div class="font-bold">{song.title}</div>
+                    <a class="link link-hover text-sm opacity-80" href={`/songs/games/${song.game.id}`}>{song.game.title}</a>
+                </div>
+            </div>
+            <PlayButton handlePlayButtonClick={() => handlePlayButtonClick(song)} />
+        </div>
+        <div class="divider divider-neutral"></div>
+    {/each}
+</div>
--- src/routes/songs/games/[gameId]/+page.svelte
+++ src/routes/songs/games/[gameId]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import Head from "$lib/components/head.svelte";
-    import SongTable from "$lib/components/song-table.svelte";
+    import SongList from "$lib/components/song-list.svelte";
     import { PAGE_TITLE } from "$lib/constant";
-    import { player, play, pause } from "$lib/stores/player.js";
+    import { play } from "$lib/stores/player.js";
     import type { Song } from "$lib/type";
 
     const { data } = $props();
@@ -11,17 +11,13 @@
         play(song)
     }
 
-    function handlePause() {
-        pause();
-    }
-
 </script>
 
 <Head title={`${data.game.title} | ${PAGE_TITLE}`} />
 
 <div class="container mx-auto">
-    <div class="flex justify-center mb-5">
+    <div class="flex justify-center mb-7">
         <h1 class="text-lg font-bold">{data.game.title}の楽曲</h1>
     </div>
-    <SongTable songs={data.songs} playingSongId={$player.song?.id && !$player.paused ? $player.song.id : null} handlePlayButtonClick={handlePlay} />
+    <SongList songs={data.songs} handlePlayButtonClick={handlePlay} />
 </div>
