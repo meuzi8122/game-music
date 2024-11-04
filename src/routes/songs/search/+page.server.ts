49fe454f9eec3a66b394b4e21e2b33608d091e65
meuzi8122 <kagiakete1234@gmail.com>
2024-11-04 19:58:01 +0900

    曲とゲーム両方のタイトルで検索

--- src/routes/songs/search/+page.server.ts
+++ src/routes/songs/search/+page.server.ts
@@ -1,12 +1,26 @@
+import { GameClient } from "$lib/clients/game";
 import { SongClient } from "$lib/clients/song";
 import { redirect } from "@sveltejs/kit";
 
 /** @type {import("./$types").PageLoad} */
 export async function load({ url }) {
-    const keyword = url.searchParams.get("keyword") || "";
+    const keyword = url.searchParams.get("keyword");
 
-    return {
-        keyword,
-        songs: await SongClient.findSongs(keyword)
+    if (keyword) {
+        let songs = await SongClient.findSongs(keyword);
+        let songIds = new Set(songs.map(song => song.id));
+
+        for (const game of await GameClient.findGames(keyword)) {
+            for (const song of await SongClient.findSongsByGameId(game.id)) {
+                if (!songIds.has(song.id)) {
+                    songs.push(song);
+                }
+                songIds.add(song.id);
+            }
+        }
+
+        return { keyword, songs }
     }
+    
+    redirect(301, "/");
 }
