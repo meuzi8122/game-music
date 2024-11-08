60cc221114df51aa1ad11da0d8ebc05adc9408af
meuzi8122 <kagiakete1234@gmail.com>
2024-11-08 17:48:53 +0900

    fix type comment

--- src/routes/api/songs/[songId]/+server.ts
+++ src/routes/api/songs/[songId]/+server.ts
@@ -1,7 +1,7 @@
 import { SongClient } from "$lib/clients/song";
 import { json } from "@sveltejs/kit";
 
-/** @type {import(""./$types").RequestHandler} */
+/** @type {import("./$types").RequestHandler} */
 export async function PATCH({ params }) {
     await SongClient.updatePlayCount(params.songId);
     return json({});
